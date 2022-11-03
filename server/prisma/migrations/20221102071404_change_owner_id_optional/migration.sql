6c2eb07eef88eb232e5823f14845e3824a8b3c46
Bruno Fukumori <brunofukumori@gmail.com>
2022-11-03 00:07:51 -0300

    initial commit: initial server and mobile and finished web

---
+++ server/prisma/migrations/20221102071404_change_owner_id_optional/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Pool" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "ownerId" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Pool_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Pool" ("code", "createdAt", "id", "ownerId", "title") SELECT "code", "createdAt", "id", "ownerId", "title" FROM "Pool";
+DROP TABLE "Pool";
+ALTER TABLE "new_Pool" RENAME TO "Pool";
+CREATE UNIQUE INDEX "Pool_code_key" ON "Pool"("code");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
